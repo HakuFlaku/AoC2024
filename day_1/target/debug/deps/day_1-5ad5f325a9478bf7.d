d1a0df0b95d7587d346a38d83bbfde44e4298f6e
Alexis <alexis.lvisser@gmail.com>
2024-11-30 22:04:47 -0600

    init of day 1

---
+++ day_1/target/debug/deps/day_1-5ad5f325a9478bf7.d
@@ -0,0 +1,5 @@
+/mnt/b8d97267-9935-4b76-af84-27e0516a26ec/git/advent_of_code_2024/day_1/target/debug/deps/libday_1-5ad5f325a9478bf7.rmeta: src/main.rs
+
+/mnt/b8d97267-9935-4b76-af84-27e0516a26ec/git/advent_of_code_2024/day_1/target/debug/deps/day_1-5ad5f325a9478bf7.d: src/main.rs
+
+src/main.rs:
