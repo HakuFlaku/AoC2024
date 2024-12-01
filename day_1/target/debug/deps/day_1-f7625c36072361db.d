19005427aa35352bad6a6efded360ec93a002eb7
Alexis <alexis.lvisser@gmail.com>
2024-11-30 22:06:19 -0600

    Remove what wasn't supposed to be included

--- day_1/target/debug/deps/day_1-f7625c36072361db.d
+++
@@ -1,5 +0,0 @@
-/mnt/b8d97267-9935-4b76-af84-27e0516a26ec/git/advent_of_code_2024/day_1/target/debug/deps/libday_1-f7625c36072361db.rmeta: src/main.rs
-
-/mnt/b8d97267-9935-4b76-af84-27e0516a26ec/git/advent_of_code_2024/day_1/target/debug/deps/day_1-f7625c36072361db.d: src/main.rs
-
-src/main.rs:
