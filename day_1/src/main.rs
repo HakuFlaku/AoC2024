3d33e39d1e829248f22a0621be96c1ea97920c54
Alexis <alexis.lvisser@gmail.com>
2024-11-30 23:56:55 -0600

    And that's problem 2 solved. Probably coulda modularized a bit better to save on some work, alas, it's a toy problem, and so I won't bother.

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,15 +1,55 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main()
 {
-    let dif = find_dif();
-    
-    println!("Total difference: {}", dif);
+    // let dif = find_dif();
+    // 
+    // println!("Total difference: {}", dif);
+
+    let similarity = find_similarity();
+
+    println!("Total similarity: {}", similarity);
 }
 
+fn find_similarity() -> i32
+{
+    let mut source = Vec::new();
+    let mut compare_map = HashMap::new();
 
+    if let Ok(lines) = read_lines("./src/input.txt")
+    {
+        for line in lines.flatten()
+        {
+            let split_vals: Vec<&str> = line.split("   ").collect();
+            let left = split_vals[0].parse::<i32>().unwrap();
+            let right = split_vals[1].parse::<i32>().unwrap();
+
+            source.push(left);
+            let count = compare_map.entry(right).or_insert(0);
+            *count += 1;
+        }
+    }
+
+    let source = source;
+    let compare_map = compare_map;
+    
+    let mut similarity_score = 0;
+    for val in source
+    {
+        match compare_map.get(&val)
+        {
+            Some(multiplier) => similarity_score += val * multiplier,
+            _ => (),
+        }
+    }
+
+    return similarity_score;
+}
+
+#[allow(dead_code)]
 fn find_dif() -> i32
 {
     // read in the input
