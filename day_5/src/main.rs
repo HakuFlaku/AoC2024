72e4ba60525b6dfdfb28171300f843d266fcb3f5
Alexis <alexis.lvisser@gmail.com>
2025-03-03 20:49:14 -0600

    Added solution for day 5 part 2. My solution was to attempt moving the problem piece to the end of the list and continually doing so till the manual is good. This only works so long as EVERY manual can be correctly ordered.

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -14,12 +14,83 @@ fn main() {
 
 	let (rules, data) = extract_puzzle(input);
 
-	let problem_1 = process_puzzle(rules, data);
+//	let problem_1 = process_puzzle_problem_1(rules, data);
+//
+//	println!("Solution for problem 1 is: {problem_1}")
 
-	println!("Solution for problem 1 is: {problem_1}")
+	let problem_2 = process_puzzle_problem_2(&rules, data);
+
+	println!("Solution for problem 2 is: {problem_2}");
 }
 
-fn process_puzzle(rules: Vec<Rule>, data: Vec<Vec<u32>>) -> u32 {
+fn process_puzzle_problem_2(rules: &Vec<Rule>, manuals: Vec<Vec<u32>>) -> u32 {
+	let mut running_total = 0;
+
+	for manual in filter_for_invalid_manuals(rules, manuals) {
+		debug!("Analyzing the manual to find a valid format or not {:?}.", &manual);
+		let mut fixed_manual = attempt_fix_to_manual(rules, &manual);
+		
+		while validate_manual(rules, &fixed_manual) == 0 {
+			fixed_manual = attempt_fix_to_manual(rules, &fixed_manual);
+		}
+		debug!("Finished re-ordering manual, final ordering is: {:?}.", &fixed_manual);
+
+		running_total += *fixed_manual.index(fixed_manual.len()/2);
+		debug!("**** Our running total is now {:?}.", &running_total);
+	}
+
+	running_total
+}
+
+fn filter_for_invalid_manuals(rules: &Vec<Rule>, manuals: Vec<Vec<u32>>) -> Vec<Vec<u32>> {
+	let mut invalid_manuals = vec![];
+
+	for manual in manuals {
+		debug!("Checking if the manual {:?} is valid.", &manual);
+
+		if validate_manual(rules, &manual) == 0 {
+			invalid_manuals.push(manual);
+		}
+	}
+
+	invalid_manuals
+}
+
+fn attempt_fix_to_manual(rules: &Vec<Rule>, manual: &Vec<u32>) -> Vec<u32> {
+	let mut failed_rule_index: Option<usize> = None;
+
+	for i in 0..manual.len() - 1 {
+		let page = manual.get(i).unwrap();
+
+		let rules_for_page: Vec<u32> = rules::find_rules_for(&rules, *page)
+			.iter()
+			.map(|x| x.before)
+			.collect();
+
+		for remainder_page in &manual[i..manual.len()] {
+			if rules_for_page.contains(remainder_page) {
+				failed_rule_index = Some(i);
+				break;
+			}
+		}
+
+		if failed_rule_index.is_some() {
+			break;
+		}
+	}
+
+	let mut re_ordered_manual = manual.clone();
+	if failed_rule_index.is_some() {
+		debug!("The index of the first page that failed a rule is: {:?}.", &failed_rule_index.unwrap());
+		let end_val = re_ordered_manual.remove(failed_rule_index.unwrap());
+		re_ordered_manual.push(end_val);
+	}
+	debug!("The re-ordered manual is {:?}.", &re_ordered_manual);
+
+	re_ordered_manual
+}
+
+fn process_puzzle_problem_1(rules: Vec<Rule>, data: Vec<Vec<u32>>) -> u32 {
 	let mut running_total = 0;
 
 	for manual in data {
